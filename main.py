ea0eeb7529e8c801b913f192d792ff5afab2b21b
Bettina <bettina.l.hein@gmail.com>
2021-08-25 12:13:45 -0400

    rewrite main into function

--- main.py
+++ main.py
@@ -1,20 +1,63 @@
 #!/usr/bin/python
 import numpy as np
 
-import matplotlib.pyplot as plt
+from tools import tools,params,three_step_method,N,DA,rhs
+
+
+def run_simulation(rA,rI,rC):
+	# s_init = arbor*(1 + params["s_noise"]*np.random.choice([-1,1],(N*N,N*N,2)))
+	# s_init = arbor * (1 + params["s_noise"] * system.rng.choice([-1,1],(N*N,N*N,2)))
+	s_init = tools.create_RFs("initialize",s_noise=params["s_noise"],arbor=arbor,N=N,\
+							  rng=np.random.RandomState(2021))#20210311
+	# s_init = tools.create_RFs("gabor",N=N,DA=DA)
+	## 20210: perfect R
+	## 202103: almost perfect R, but on/off different sizes
+
+	s_init *= rhs.synaptic_normalization_factor_init(s_init,arbor,mode_norm)
+	## step 5 (see Miller 1994)
+	s_init,frozen_init = rhs.clip_synapse(s_init,params["s_max"],arbor,\
+											np.zeros_like(s_init,dtype=bool))
+	## step 1 (see Miller 1994)
+	delta,lambda_normalised = rhs.unconstrained_init(s_init,0,params["lambda_init"],i_fct,\
+														arbor,c_onon,c_onoff,c_offoff,\
+														params["target_sigma"],mode_norm)
+
+	lambda_normalised = lambda_normalised
+	print("lambda_normalised",lambda_normalised)
+	## step 2 (see Miller 1994)
+	s1,st,frozen_ratio,frozen = three_step_method.three_step_method(s_init,\
+																	0,\
+																	lambda_normalised,\
+																	rhs.constrained,\
+																	frozen_init,\
+																	i_fct,\
+																	arbor,\
+																	c_onon,\
+																	c_onoff,\
+																	c_offoff,\
+																	params["s_max"],\
+																	c_orth,s_orth,\
+																	mode_norm)
+	print('frozen ratio at {}.'.format(frozen_ratio[-1]))
+	print("s1",s1.shape,st.shape)
+	return s1
+
+
 
 if __name__=="__main__":
-	from tools import parse_args,tools,plotting,network_system,params,three_step_method,\
-	N,DA,interaction_params,LGN_corr_params,rhs
+	import matplotlib.pyplot as plt
+	from tools import parse_args,network_system,interaction_params,LGN_corr_params
 
-	mode_norm = "x"
+	mode_norm = "alpha"
 	c_orth,s_orth  = None,None
 
-	version = 28#tools.get_version_id()
+	version = 83#tools.get_version_id()
 	print("version",version)
 	rA = params["rA"]
 	rI = parse_args.args.intracortical_scale * rA
 	rC = parse_args.args.LGNinput_scale * rA
+
+	print(rA,rI,rC)
 
 	
 	N = params["N"]
@@ -22,9 +65,15 @@ if __name__=="__main__":
 	
 	system = network_system.Network((N,N),(N,N),version)
 
+	## Gaussian recurrent connectivity
 	connectivity_params = {"ampl" : interaction_params["ampl"], "sigma" : rI}
 	xI = np.min([2*rI,N//2])
 	i_fct = system.create_matrix(connectivity_params,interaction_params["profile"],rA=xI)
+	## MH recurrent connectivity
+	# connectivity_params = {"ampl" : interaction_params["ampl"], "sigma1" : rI, "sigma2" : rI*2}
+	# xI = np.min([2*rI,N//2])
+	# i_fct = system.create_matrix(connectivity_params,"Mexican-hat",rA=xI)
+
 
 	corr_params = {"ampl" : LGN_corr_params["ampl"], "sigma" : rC}
 	c_onon = system.create_matrix(corr_params,LGN_corr_params["profile"])
@@ -32,7 +81,7 @@ if __name__=="__main__":
 	corr_params = {"ampl" : -0.5*LGN_corr_params["ampl"], "sigma" : rC}
 	c_onoff = system.create_matrix(corr_params,LGN_corr_params["profile"])
 
-	arbor_sgl = system.create_arbor(radius=rA,profile="heaviside")
+	arbor_sgl = system.create_arbor(radius=rA,profile="gaussian")
 	arbor = np.dstack([arbor_sgl,arbor_sgl])
 	print("SHAPE",arbor.shape,c_onon.shape,i_fct.shape)
 
@@ -78,62 +127,8 @@ if __name__=="__main__":
 	# exit()
 	###################################################################################3
 
-
-	# np.random.seed(20191218)
-	# s_init = arbor*(1 + params["s_noise"]*np.random.choice([-1,1],(N*N,N*N,2)))
-	# s_init = arbor * (1 + params["s_noise"] * system.rng.choice([-1,1],(N*N,N*N,2)))
-	s_init = tools.create_RFs("initialize",s_noise=params["s_noise"],arbor=arbor,N=N)
-	# s_init = tools.create_RFs("gabor",N=N,DA=DA)
-
-	s_init *= rhs.synaptic_normalization_factor_init(s_init,arbor)
-	# print("s_init",np.nanmax(s_init),np.nanmin(s_init))
-	# exit()
-	## step 5 (see Miller 1994)
-	s_init,frozen_init = rhs.clip_synapse(s_init,params["s_max"],arbor,\
-							np.zeros_like(s_init,dtype=bool))
-	## step 1 (see Miller 1994)
-	delta,lambda_normalised = rhs.unconstrained_init(s_init,0,params["lambda_init"],i_fct,\
-								arbor,c_onon,c_onoff,c_offoff,params["target_sigma"])
-
-	lambda_normalised = lambda_normalised# * 0.05
-	print("lambda_normalised",lambda_normalised)
-	## step 2 (see Miller 1994)
-	s1,st,frozen_ratio,frozen = three_step_method.three_step_method(s_init,\
-																0,\
-																lambda_normalised,\
-																rhs.constrained,\
-																frozen_init,\
-																i_fct,\
-																arbor,\
-																c_onon,\
-																c_onoff,\
-																c_offoff,\
-																params["s_max"],\
-																c_orth,s_orth,\
-																mode_norm)
-	print('frozen ratio at {}.'.format(frozen_ratio[-1]))
-	print("s1",s1.shape,st.shape)
-
-	# fig = plt.figure()
-	# ax = fig.add_subplot(141)
-	# im=ax.imshow(s1[:,:,0],interpolation="nearest",cmap="binary")
-	# plt.colorbar(im,ax=ax)
-	# ax = fig.add_subplot(142)
-	# im=ax.imshow(s1[0,:,0].reshape(N,N),interpolation="nearest",cmap="binary")
-	# plt.colorbar(im,ax=ax)
-	# ax = fig.add_subplot(143)
-	# im=ax.imshow(s1[0,:,1].reshape(N,N),interpolation="nearest",cmap="binary")
-	# plt.colorbar(im,ax=ax)
-	# ax = fig.add_subplot(144)
-	# im=ax.imshow(s1[310,:,0].reshape(N,N),interpolation="nearest",cmap="binary")
-	# plt.colorbar(im,ax=ax)
-	# plt.show()
-	# exit()
-
-	##do plotting
-	# plotting.plot_figures(st,N=N,version=version,DA=params["DA"]+2,arbor=arbor_sgl)
-
+	s1 = run_simulation(rA,rI,rC)
 	results_dict = {"rC" : rC, "rI" :  rI, "s" : s1, "rA" : rA, "N" : N}
-	# tools.write_to_hdf5(results_dict,version)
+	tools.write_to_hdf5(results_dict,version)
 
 	print("done")
